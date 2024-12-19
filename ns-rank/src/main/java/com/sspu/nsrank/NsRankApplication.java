046fa1570daa712bcbf8cb6d8cf2876044b1f815
成东杰 <kariseven323@gmail.com>
2024-12-19 20:57:02 +0800

    ns-rank模块部署（测试通过)（逻辑暂未优化）

--- ns-rank/src/main/java/com/sspu/nsrank/NsRankApplication.java
+++ ns-rank/src/main/java/com/sspu/nsrank/NsRankApplication.java
@@ -3,14 +3,13 @@ package com.sspu.nsrank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableMongoRepositories(basePackages = "com.sspu.nslike.repository") // 指定 Repository 包路径
+@EnableDiscoveryClient // 启用服务发现，适用于微服务架构中Eureka或其他注册中心
 public class NsRankApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(NsRankApplication.class, args);
-        System.out.println("Ns-Rank 模块启动成功！");
+        System.out.println("Ns-Rank 模块成功启动！");
     }
 }
